813ded27de53419ebfb2d0fc3a41c12d8c4c3472
Patricio Leiva Ferrer <pleiva1138gmail.com>
2024-10-15 16:08:25 -0300

    Actualización

--- src/app/pages/principal/principal.page.ts
+++ src/app/pages/principal/principal.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-principal',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    console.log("Principal")
+  }
+
+  cerrarSesion() {
+    this.router.navigate(['login'])
   }
 
 }
